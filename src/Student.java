05323252200d856043b26b569f7f859b6dc7ee2c
GilMcv <118422096+GilMcv@users.noreply.github.com>
2024-04-03 21:35:19 +0800

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Student {
+public class Student extends LibraryUser {
+
+    @Override
+    public void printItemsBorrowed() {
+        System.out.println("Student has borrowed the following items:");
+        for (LibraryItem item : borrowedItems) {
+            if (item instanceof Book) {
+                Book book = (Book) item;
+                System.out.println("Book: '" + book.getTitle() + "' by " + book.getAuthor());
+            } else if (item instanceof DVD) {
+                DVD dvd = (DVD) item;
+                System.out.println("DVD: '" + dvd.getTitle() + "' directed by " + dvd.getDirector());
+            }
+        }
+    }
 }
