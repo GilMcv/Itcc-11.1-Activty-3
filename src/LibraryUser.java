541efbf12e50a53373c02b80ed40e42e7af483aa
GilMcv <118422096+GilMcv@users.noreply.github.com>
2024-04-03 21:35:02 +0800

    Update LibraryUser.java

--- src/LibraryUser.java
+++ src/LibraryUser.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class LibraryUser {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class LibraryUser {
+    protected List<LibraryItem> borrowedItems;
+
+    public LibraryUser() {
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public void borrowItem(LibraryItem item) {
+        item.borrowItem();
+        borrowedItems.add(item);
+    }
+
+    public void returnItem(LibraryItem item) {
+        item.returnItem();
+        borrowedItems.remove(item);
+    }
+
+    public abstract void printItemsBorrowed();
 }
