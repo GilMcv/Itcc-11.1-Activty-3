69ca055af243d83c76691ad1e02402c6ba2abde8
GilMcv <118422096+GilMcv@users.noreply.github.com>
2024-04-03 21:32:28 +0800

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Book {
+public class Book implements LibraryItem {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean borrowed;
+
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.borrowed = false;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (!borrowed) {
+            borrowed = true;
+            System.out.println("Book '" + title + "' by " + author + " has been borrowed.");
+        } else {
+            System.out.println("Book is already borrowed.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        if (borrowed) {
+            borrowed = false;
+            System.out.println("Book '" + title + "' by " + author + " has been returned.");
+        } else {
+            System.out.println("Book is not borrowed.");
+        }
+    }
+
+    @Override
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
 }
