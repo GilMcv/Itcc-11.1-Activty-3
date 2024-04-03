f6b5ee20b8b1baeb8405d7ec7aa8ad0aa64b1c6e
GilMcv <118422096+GilMcv@users.noreply.github.com>
2024-04-03 21:34:23 +0800

    Update DVD.java

--- src/DVD.java
+++ src/DVD.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class DVD {
+public class DVD implements LibraryItem {
+    private String title;
+    private String director;
+    private int runningTime;
+    private boolean borrowed;
+
+    public DVD(String title, String director, int runningTime) {
+        this.title = title;
+        this.director = director;
+        this.runningTime = runningTime;
+        this.borrowed = false;
+    }
+
+    @Override
+    public void borrowItem() {
+        if (!borrowed) {
+            borrowed = true;
+            System.out.println("DVD '" + title + "' directed by " + director + " has been borrowed.");
+        } else {
+            System.out.println("DVD is already borrowed.");
+        }
+    }
+
+    @Override
+    public void returnItem() {
+        if (borrowed) {
+            borrowed = false;
+            System.out.println("DVD '" + title + "' directed by " + director + " has been returned.");
+        } else {
+            System.out.println("DVD is not borrowed.");
+        }
+    }
+
+    @Override
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRunningTime() {
+        return runningTime;
+    }
 }
